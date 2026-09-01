a4f42ec419b497db59d08f3bf3a7fdc3a8fe066b
Mohammad Araf <159543785+Araf1011@users.noreply.github.com>
2026-09-01 22:14:35 +0600

    Delete serializer.py

--- serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length = 255)
-    roll = serializers.IntegerField()
-    section = serializers.CharField(max_length= 20)
