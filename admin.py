494263664a686c86e0f62013c38968b2bd55fa0a
Mohammad Araf <159543785+Araf1011@users.noreply.github.com>
2026-09-01 13:17:26 +0600

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Student
+
+@admin.register(Student)
+class Student(admin.ModelAdmin):
+    list_display = ['id','name','roll','section']
+
+# Register your models here.
